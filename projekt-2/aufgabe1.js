9526d662a2583e6f60f18958ed4ba700f0f1be2d
Hakan9092 <hakanekinci.hb@gmail.com>
2021-06-12 13:07:20 +0200

    projekt2-work

---
+++ projekt-2/aufgabe1.js
@@ -0,0 +1,8 @@
+function addieren() {
+    var zahl1 = parseInt(document.getElementById("addieren1").value);
+    var zahl2 = parseInt(document.getElementById("addieren2").value);
+
+
+    document.getElementById("ergebnis").innerHTML = zahl1 + zahl2;
+
+}
